9299cad0be220bac0396a46356c45b99cd450f2c
Harry Sauers <hsauers5@gmail.com>
2021-03-22 12:04:19 -0400

    Update App.js
    Removed unnecessary code

--- brainbeatsfront/src/App.js
+++ brainbeatsfront/src/App.js
@@ -1,40 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    const mongoose = require("mongoose");
-
-    mongoose.connect("mongodb://127.0.0.1:27017/main", {
-        useNewUrlParser: true
-    });
-
-    const connection = mongoose.connection;
-
-    connection.once("open", function() {
-        console.log("Connection with MongoDB was successful");
-    });
-
-    let detail = require("./model");
-    
-    /*
-    detail.find({}, function(err, result) {
-            if (err) {
-                res.send(err);
-            } else {
-                res.send(result);
-            }
-        });
-    */
-    
+function App() {    
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-        
         </p>
         <a
           className="App-link"
