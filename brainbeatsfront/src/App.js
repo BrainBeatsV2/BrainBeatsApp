68ee5413f2517afc9f977a154906a2a2a4204198
hsauers5@gmail.com <hsauers5@gmail.com>
2021-03-22 12:02:21 -0400

    Added dummy data for DB

--- brainbeatsfront/src/App.js
+++ brainbeatsfront/src/App.js
@@ -2,12 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const mongoose = require("mongoose");
+
+    mongoose.connect("mongodb://127.0.0.1:27017/main", {
+        useNewUrlParser: true
+    });
+
+    const connection = mongoose.connection;
+
+    connection.once("open", function() {
+        console.log("Connection with MongoDB was successful");
+    });
+
+    let detail = require("./model");
+    
+    /*
+    detail.find({}, function(err, result) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send(result);
+            }
+        });
+    */
+    
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+        
         </p>
         <a
           className="App-link"
