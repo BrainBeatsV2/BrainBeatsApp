7f654e6b746f65aa3eb945e26322fdbf1a8743d6
hsauers5@gmail.com <hsauers5@gmail.com>
2021-03-23 13:39:23 -0400

    Fixed dockerized restructuring of app

--- brainbeatsback/server.js
+++ brainbeatsback/server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const PORT = 3000
+const PORT = 4000
 
 dotenv.config()
 
-mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log("Database connected"))
+mongoose.connect('mongodb://<MONGO_USERNAME>:<MONGO_PASSWORD>@<MONGO_HOSTNAME>:<MONGO_PORT>/<MONGO_DB>', () =>console.log("DB Connected Successfully"));
 
 app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.listen(PORT, () => console.log("Running on"), PORT)
+app.listen(PORT, () => console.log("Running on"), PORT);
