a42ab933ef8386dfa929c55df12558c72661cd17
hsauers5@gmail.com <hsauers5@gmail.com>
2021-03-23 14:58:35 -0400

    Database auth thru Mongoose & env vars

--- brainbeatsback/server.js
+++ brainbeatsback/server.js
@@ -11,9 +11,10 @@ var db = process.env.MONGO_DB ;
 
 var mongo_uri = 'mongodb://' + username + ':' + password + '@' + host + ':' + port + '/' + db;
 
+var db;
 function connectToDB() {
     mongoose.connect(mongo_uri, { useNewUrlParser: true });
-    var db =  mongoose.connection;
+    db =  mongoose.connection;
     db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 }
 
@@ -21,6 +22,20 @@ setTimeout(connectToDB, 5000);
 
 app.get('/', (req, res) => {
     res.send("hello world");
+})
+
+app.get('/db', (req, res) => {
+    var users = db.collection("users").find({}).toArray();
+    var midi = db.collection("midi").find({}).toArray();
+    var model = db.collection("model").find({}).toArray();
+
+    var data = {
+        "users": users, 
+        "midi": midi, 
+        "model": model,
+    }
+
+    res.send(data);
 })
 
 app.listen(PORT, () => console.log("Running on"), PORT);
