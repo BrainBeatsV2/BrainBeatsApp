03496869698a37130c9a226d1d184d1c1146b191
hsauers5@gmail.com <hsauers5@gmail.com>
2021-04-07 15:42:24 -0400

    Implemented 'Download All' functionality for models

--- brainbeatsback/server.js
+++ brainbeatsback/server.js
@@ -142,6 +142,25 @@ app.get('/api/models', function(req, res) {
 
 /*
     Example: 
+        GET localhost:4000/api/models/all
+        Headers- Content-Type: application/json; charset=utf-8
+        Response- 200 OK
+    * You MUST supply the exact Content-Type above, or it won't work. 
+*/
+app.get('/api/models/all', function(req, res) {
+    var conn = getConnection();
+
+    Model.find().then(function(doc) {
+        if (doc == null) {
+            res.status(404).send("No models found.");
+        } else {
+            res.status(200).send(doc);
+        }
+    });
+})
+
+/*
+    Example: 
         GET localhost:4000/api/models/MODEL_NAME
         Headers- Content-Type: application/json; charset=utf-8
         Response- 200 OK
