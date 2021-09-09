4afcffa4749393d73bf7889b1e71762d0333b983
Melanie Brady <melaniebrady@knights.ucf.edu>
2021-09-09 00:45:26 -0700

    Updated file structure & frontend changes

--- brainbeatsfront/src/App.js
+++ brainbeatsfront/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {    
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages';
+import Login from './pages/login';
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/login" exact component={Login} />
+            </Switch>
+        </Router>
+
+        // Default return for React
+        // <div className="App">
+        //     <header className="App-header">
+        //         <img src={logo} className="App-logo" alt="logo" />
+        //         <p>
+        //             Edit <code>src/App.js</code> and save to reload.
+        //         </p>
+        //         <a
+        //             className="App-link"
+        //             href="https://reactjs.org"
+        //             target="_blank"
+        //             rel="noopener noreferrer"
+        //         >
+        //             Learn React
+        //         </a>
+        //     </header>
+        // </div>
+    );
 }
 
 export default App;
