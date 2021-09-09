187274fabdbc11e9017258fe47e39ba4331441aa
Melanie Brady <melaniebrady@knights.ucf.edu>
2021-09-09 11:37:50 -0700

    Adding files & libraries

---
+++ brainbeatsfront/src/scripts/midi_index.js
@@ -0,0 +1,143 @@
+// module.exports.exports = {
+//   parseCSV: parseCSV() {
+//   }
+
+// eegData = parseCSV('/eeg_data.csv');
+// console.log('Finished parsing');
+// console.log(eegData);
+const csv = require('csvtojson')
+
+
+generateMidi('/eeg_data.csv', 100);
+
+async function parseCSV(filePath) {
+  // var fs = require('fs');
+  // var csv = require('csv-parser');
+  // var data = [];
+
+  // fs.readFileSync('eeg_data.csv')
+  //   .pipe(csv())
+  //   .on('data', (row) => {
+  //     console.log(row)
+  //     data.push(row)
+  //   })
+  //   .on('end', function () {
+  //     console.log('CSV file has been read')
+  //   })
+  const csvFilePath = 'eeg_data.csv'
+  const array = await csv().fromFile(csvFilePath);
+
+  return (array)
+};
+
+async function generateMidi(filePath, time) {
+
+  eegData = await parseCSV(filePath)
+  console.log('Finished parsing. Here is the data')
+  // let average = (array) => array.reduce((a, b) => a + b) / array.length;
+
+  console.log(eegData)
+  // 
+  deltaAvg = average(eegData[0].Delta)
+  thetaAvg = average(eegData[0])
+  alphaAvg = average(eegData[0])
+  betaAvg = average(eegData[0])
+  gammaAvg = average(eegData[0])
+
+  var MidiWriter = require('midi-writer-js')
+
+  // Start with a new track
+  var track = new MidiWriter.Track();
+
+  // Define an instrument (optional):
+  track.addEvent(new MidiWriter.ProgramChangeEvent({ instrument: 1 }));
+
+  var intervalPitchMap = new Map();
+  intervalPitchMap.set(1, 'C4');
+  intervalPitchMap.set(2, 'D4');
+  intervalPitchMap.set(3, 'E4');
+  intervalPitchMap.set(4, 'G4');
+  intervalPitchMap.set(5, 'A4');
+
+  var noteGroupings = [1, 2, 3, 4, 6, 8];
+  var durations = ['4', '8', '8t', '16', '16t', '32'];
+
+  let buildDistribution = (currentPitch) => {
+    for (var j = 0; j < numberOfNotesToGenerate; j++) {
+      distribution = [];
+      // most common pitches will be one note up or one note down
+      for (var k = 0; k < 25; k++) {
+        if (currentPitch > 1) {
+          distribution.push(currentPitch - 1);
+        } else {
+          distribution.push(currentPitch + 1);
+        }
+      }
+
+      for (var k = 0; k < 25; k++) {
+        if (currentPitch < 5) {
+          distribution.push(currentPitch + 1);
+        } else {
+          distribution.push(currentPitch - 1);
+        }
+      }
+      // skips will be slightly less common
+      for (var k = 0; k < 15; k++) {
+        if (currentPitch < 4) {
+          distribution.push(currentPitch + 2);
+        } else {
+          distribution.push(currentPitch - 2);
+        }
+      }
+
+      for (var k = 0; k < 15; k++) {
+        if (currentPitch > 2) {
+          distribution.push(currentPitch - 2);
+        } else {
+          distribution.push(currentPitch + 2);
+        }
+      }
+
+      // staying on the same note is also possible
+      for (var k = 0; k < 20; k++) {
+        distribution.push(currentPitch);
+      }
+    }
+    return distribution;
+  }
+
+  var noteEvents = [];
+
+  var currentPitch = 3;
+  for (var i = 0; i < time; i++) {
+    // pick a random note grouping and duration and generate that many notes
+    var numberOfNotesToGenerate = noteGroupings[Math.floor(Math.random() * noteGroupings.length)];
+    var duration = durations[Math.floor(Math.random() * durations.length)];
+    // console.log(duration.toString());
+    var pitches = [];
+    for (var j = 0; j < numberOfNotesToGenerate; j++) {
+      var distribution = buildDistribution(currentPitch);
+      var randomIndex = Math.floor(Math.random() * 100);
+      var nextPitch = distribution[randomIndex];
+      pitches.push(nextPitch);
+      currentPitch = nextPitch;
+    }
+
+    var pitchesAsNotes = [];
+    pitches.forEach(pitch => {
+      // console.log(pitch);
+      pitchesAsNotes.push(intervalPitchMap.get(pitch));
+    });
+    noteEvents.push(new MidiWriter.NoteEvent({ pitch: pitchesAsNotes, duration: duration.toString() }));
+  }
+
+  // Add some notes:
+  track.addEvent(noteEvents, function (event, index) {
+    return { sequential: true };
+  });
+
+  // Generate a data URI
+  var write = new MidiWriter.Writer(track);
+  // console.log(write.dataUri());
+  write.saveMIDI('file');
+}
