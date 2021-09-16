ef40dbf5e64236e1936d5e7284101094c1657ee8
MelanieBrady <melaniebrady@knights.ucf.edu>
2021-09-16 10:59:04 -0700

    running into more electron issues saving to switch to master

--- brainbeatsfront/public/electron.js
+++ brainbeatsfront/public/electron.js
@@ -32,67 +32,67 @@ app.on('activate', () => {
 
 
 
-// Event listeners for coordinating IPC between main and renderer threads
-let pyshell
+// // Event listeners for coordinating IPC between main and renderer threads
+// let pyshell
 
-const endPyshell = _ => {
-  if (pyshell == null || pyshell == undefined) {
-    return
-  }
-  console.log('BACKGROUND DEBUG PRINT: Ending Script Child Process')
-  pyshell.childProcess.kill(0)
-  pyshell = null
-}
+// const endPyshell = _ => {
+//   if (pyshell == null || pyshell == undefined) {
+//     return
+//   }
+//   console.log('BACKGROUND DEBUG PRINT: Ending Script Child Process')
+//   pyshell.childProcess.kill(0)
+//   pyshell = null
+// }
 
-const parsePyshellMessage = args => {
-  try {
-    const messageDetails = JSON.parse(args)
-    if (messageDetails == undefined || messageDetails == null) {
-      return
-    }
+// const parsePyshellMessage = args => {
+//   try {
+//     const messageDetails = JSON.parse(args)
+//     if (messageDetails == undefined || messageDetails == null) {
+//       return
+//     }
 
-    /// Handle sending emotion
-    if (messageDetails.emotion != null && messageDetails.emotion != undefined) {
-      console.log('BACKGROUND DEBUG PRINT: Emotion Predicted')
-      mainWindow.webContents.send('HARDWARE_PROCESS_MESSAGE', messageDetails.emotion)
-      return
-    }
+//     /// Handle sending emotion
+//     if (messageDetails.emotion != null && messageDetails.emotion != undefined) {
+//       console.log('BACKGROUND DEBUG PRINT: Emotion Predicted')
+//       mainWindow.webContents.send('HARDWARE_PROCESS_MESSAGE', messageDetails.emotion)
+//       return
+//     }
 
-    /// Handle sending confirmation
-    if (messageDetails.hasConfirmed != undefined && messageDetails.hasConfirmed != null) {
-      console.log('BACKGROUND DEBUG PRINT: Connection Confirmed')
-      return
-    }
-  } catch (error) {
-    console.log(args)
-  }
-}
+//     /// Handle sending confirmation
+//     if (messageDetails.hasConfirmed != undefined && messageDetails.hasConfirmed != null) {
+//       console.log('BACKGROUND DEBUG PRINT: Connection Confirmed')
+//       return
+//     }
+//   } catch (error) {
+//     console.log(args)
+//   }
+// }
 
-// Event to tell electron to create python script handler
-ipcMain.on('HARDWARE_PROCESS_START', event => {
-  endPyshell()
-  let startScriptPath = path.join(__dirname, 'hardware/hardware_starter.py')
-  pyshell = new PythonShell(startScriptPath, {
-    pythonPath: 'python',
-  })
+// // Event to tell electron to create python script handler
+// ipcMain.on('HARDWARE_PROCESS_START', event => {
+//   endPyshell()
+//   let startScriptPath = path.join(__dirname, 'hardware/hardware_starter.py')
+//   pyshell = new PythonShell(startScriptPath, {
+//     pythonPath: 'python',
+//   })
 
-  pyshell.on('message', function (results) {
-    parsePyshellMessage(results)
-  })
+//   pyshell.on('message', function (results) {
+//     parsePyshellMessage(results)
+//   })
 
-  pyshell.on('error', function (results) {
-    console.log('BACKGROUND DEBUG PRINT: Script Error Exit')
-    endPyshell()
-  })
+//   pyshell.on('error', function (results) {
+//     console.log('BACKGROUND DEBUG PRINT: Script Error Exit')
+//     endPyshell()
+//   })
 
-  pyshell.on('stderr', function (stderr) {
-    endPyshell()
-    console.log(stderr)
-    mainWindow.webContents.send('HARDWARE_PROCESS_ERROR')
-  })
-})
+//   pyshell.on('stderr', function (stderr) {
+//     endPyshell()
+//     console.log(stderr)
+//     mainWindow.webContents.send('HARDWARE_PROCESS_ERROR')
+//   })
+// })
 
-// Event to shutdown python script handler
-ipcMain.on('HARDWARE_PROCESS_SHUTDOWN', event => {
-  endPyshell()
-})
+// // Event to shutdown python script handler
+// ipcMain.on('HARDWARE_PROCESS_SHUTDOWN', event => {
+//   endPyshell()
+// })
