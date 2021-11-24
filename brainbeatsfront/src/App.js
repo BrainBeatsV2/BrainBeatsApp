b0db9f27561c9a81b376f94fc28cf918cc791ff4
Jordy Pantoja <pantojajo@knights.ucf.edu>
2021-11-23 22:54:52 -0500

    removed acc and settings page

--- brainbeatsfront/src/App.js
+++ brainbeatsfront/src/App.js
@@ -9,8 +9,6 @@ import Dashboard from './pages/dashboard'
 import Play from './pages/play'
 import Discover from './pages/discover'
 import Help from './pages/help'
-import Account from './pages/my-account'
-import Settings from './pages/settings'
 
 
 function App() {
@@ -38,10 +36,6 @@ function App() {
           (<Discover {...props} username={this.state.username} password={this.state.password} email={this.state.email} />)} />
         <Route path="/help" exact component={Help} render={props =>
           (<Help {...props} username={this.state.username} password={this.state.password} email={this.state.email} />)} />
-        <Route path="/settings" exact component={Settings} render={props =>
-          (<Settings {...props} username={this.state.username} password={this.state.password} email={this.state.email} />)} />
-        <Route path="/my-account" exact component={Account} render={props =>
-          (<Account {...props} username={this.state.username} password={this.state.password} email={this.state.email} />)} />
         <Route component={Home} />
       </Switch>
     </Router>
