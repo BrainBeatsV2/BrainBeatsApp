8eb682b3e18b3dbeb7de148c28ca7c71bc3ae722
Jordy Pantoja <pantojajo@knights.ucf.edu>
2021-11-29 18:57:35 -0500

    Added custom mac and serial port, help pages, and home page edit

--- brainbeatsfront/src/pages/index.js
+++ brainbeatsfront/src/pages/index.js
@@ -74,12 +74,27 @@ class Home extends Component {
                   password={this.state.password}
                 ></Sidebar>
                 <div id="main_content" class="help_screen">          
-                  <h2>Brain Beats</h2>
-                  <h3>About</h3>
-                  <h3>How To Get Started</h3>
-                  <h3>Help</h3>
+                <h2>Brain Beats</h2>
+                <br />
+                <h3>About</h3>
+                <p>Brain Beats was created to map properties of electroencephalographically (EEG) recorded
+                    brain waves to properties of music. Brain Beats provides a music composition platform to users of all music 
+                    experience levels and has the capability to generate, share, and view brain-generated music.</p>
+                    <br />
+                <h3>How To Get Started</h3>
+                <br />
+                <ul>
+                  <li>1. Download our Brain Beats Desktop application</li>
+                  <p>Compatible with Windows, Mac OS and Linux</p>
+                  <li>2. Plug-In a <a href="https://brainflow.readthedocs.io/en/stable/SupportedBoards.html" target="_blank">compatible EEG headset</a> to get started</li>
+                  <p>We have a synthetic mode that provides similar functionality if you don't have an EEG headset available.</p>
+                  <li>3. Customize your music by setting parameters</li><p>Select a model, key, scale, octave range, instrument, tempo and time signature</p>
+                  <li>4. Begin recording</li><p>You'll see a visualizer to show that you're in record mode</p>
+                  <li>5. Download, Save and Upload your MIDI</li><p>Save and Upload your MIDI to your account and share on MIDI Discover</p>
+                  </ul>
+                <br />
                 </div>
-      
+              
               <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
       
               
